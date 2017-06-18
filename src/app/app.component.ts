8dd5b1b3ce393d0687dadd9f156538468f445f59
vvendemia <vvendemia1@gmail.com>
2017-06-18 05:50:18 -0400

    Switch to inLine template in Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+	  <h1>{{title}}</h1>
+	  <h2>{{goal.title}} details!</h2>
+	  <div><label>id: </label>{{goal.id}}</div>
+	  <div><label>name: </label>{{goal.title}}</div>
+	  `
 })
 
 export class AppComponent {

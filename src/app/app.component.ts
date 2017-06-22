50d90c516fbfe20719f9700eac401d95c8d17ac7
vvendemia <vvendemia1@gmail.com>
2017-06-22 17:08:31 -0400

    Add GOALS const

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+const GOALS: Goal[] = [
+  { id: 11, title: 'Goal 1' },
+  { id: 12, title: 'Goal 2' },
+  { id: 13, title: 'Goal 3' },
+  { id: 14, title: 'Goal 4' },
+  { id: 15, title: 'Goal 5' },
+  { id: 16, title: 'Goal 6' },
+  { id: 17, title: 'Goal 7' }
+];
+
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',

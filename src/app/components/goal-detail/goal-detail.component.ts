c22608dfbc233d8044e7db03d43c10f9a8aca907
Vinnie Vendemia <vvendemia1@gmail.com>
2017-06-30 18:31:56 -0400

    Move goal-detail template to separate file

--- src/app/components/goal-detail/goal-detail.component.ts
+++ src/app/components/goal-detail/goal-detail.component.ts
@@ -3,16 +3,7 @@ import { Goal } from '../../models/goal';
 
 @Component({
   selector: 'goal-detail',
-  template: `
-  <div *ngIf="goal">
-	  <h2>{{goal.title}} details!</h2>
-	  <div><label>id: </label>{{goal.id}}</div>
-	  <div>
-      <label>Title: </label>
-      <input [(ngModel)]="goal.title" placeholder="title">
-    </div>
-  </div>
-  `
+  templateUrl: './goal-detail.component.html'
 })
 
 export class GoalDetailComponent {

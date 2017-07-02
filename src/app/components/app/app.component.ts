5d3eaf513be6638a14f64d329bb31b8ac83b8d2f
Vinnie Vendemia <vvendemia1@gmail.com>
2017-07-02 09:13:15 -0400

    Refactor AppComponent class

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,35 +1,13 @@
 // Import Angular components 
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
-// Import App Components
-import { Goal } from '../../models/goal';
-import { GoalService } from '../../services/goal.service'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [GoalService]
+  template: `
+    <h1>{{title}}</h1>
+    <my-goals></my-goals>
+  `
 })
 
-export class AppComponent implements OnInit {
-  title = 'Goals App';
-  goals: Goal[];
-  selectedGoal: Goal;
-
-  onSelect(goal: Goal): void {
-    this.selectedGoal = goal;
-  }
-
-  constructor(private goalService: GoalService) {}
-
-  getGoals(): void {
-    this.goalService.getGoals().then(goals => this.goals = goals);
-  }
-
-  ngOnInit(): void {
-    this.getGoals();
-  }
-
+export class AppComponent {
 }

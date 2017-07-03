eba736705863a682360904f55639ee2d81fd3956
Vinnie Vendemia <vvendemia1@gmail.com>
2017-07-03 19:07:17 -0400

    Import RouterModule in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+// Angular components
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
+// Application Components
 import { AppComponent } from './components/app/app.component';
 import { GoalDetailComponent } from './components/goal-detail/goal-detail.component';
 import { GoalsComponent } from './components/goals/goals.component';
@@ -14,7 +17,13 @@ import { GoalsComponent } from './components/goals/goals.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'goals',
+        component: GoalsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

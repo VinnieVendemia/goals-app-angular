3cc4f41e71090b06c7ee9f13321e254c523da5bb
vvendemia <vvendemia1@gmail.com>
2017-07-06 18:40:26 -0400

    DashboardComponent implements OnInit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+// Angular Components
+import { Component, OnInit } from '@angular/core';
+
+// App Components
+import { Goal } from '../../models/goal';
+import { GoalService } from '../../services/goal.service';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+
+	goals: Goal[] = [];
+
+	constructor(private goalService: GoalService) { }
+
+	 ngOnInit(): void {
+    this.goalService.getGoals()
+      .then(goals => this.goals = goals.slice(1, 5));
+  }
+}

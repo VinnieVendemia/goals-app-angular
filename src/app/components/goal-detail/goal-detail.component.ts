4a4134f92e386f38bdbe30d0fec16b4ec20c9463
vvendemia <vvendemia1@gmail.com>
2017-07-07 17:58:52 -0400

    Update GoalDetailComponent

--- src/app/components/goal-detail/goal-detail.component.ts
+++ src/app/components/goal-detail/goal-detail.component.ts
@@ -1,11 +1,35 @@
-import { Component, Input} from '@angular/core';
+// Angular components 
+import { Component, Input, OnInit} from '@angular/core';
+import { ActivatedRoute, ParamMap} from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+// Application Components
 import { Goal } from '../../models/goal';
+import { GoalService } from '../../services/goal.service';
 
 @Component({
   selector: 'goal-detail',
   templateUrl: './goal-detail.component.html'
 })
 
-export class GoalDetailComponent {
+export class GoalDetailComponent implements OnInit {
 	  @Input() goal: Goal
+
+	  constructor(
+	  	private goalService: GoalService,
+  		private route: ActivatedRoute,
+  		private location: Location
+		) {}
+
+		ngOnInit(): void {
+		  this.route.paramMap
+		    .switchMap(
+		    	(params: ParamMap) => this.goalService.getGoal(+params.get('id'))
+		    ).subscribe(goal => this.goal = goal);
+		}
+
+		goBack(): void {
+  		this.location.back();
+		}
 }

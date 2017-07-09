040f269f72851448aff3241917d598f3e6214f9a
vvendemia <vvendemia1@gmail.com>
2017-07-09 18:05:17 -0400

    Add HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule, Routes }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 // Application Components
 import { AppComponent } from './components/app/app.component';
@@ -23,7 +24,8 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [GoalService],
   bootstrap: [AppComponent]

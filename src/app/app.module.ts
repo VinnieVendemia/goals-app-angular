8ec517eb683cad25a1599ea4b0415bff9090cf1d
Vinnie Vendemia <vvendemia1@gmail.com>
2017-07-10 20:53:37 -0400

    Update reference to in-memory-data.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpModule }    from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService }  from './services/in-memory-data.service';
 
 // Application Components
 import { AppComponent } from './components/app/app.component';

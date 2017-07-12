3814ac9ee871f847160c7e6827258cabbc68c21f
Vinnie Vendemia <vvendemia1@gmail.com>
2017-07-11 21:48:06 -0400

    Update in-memory-data-service

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,7 +1,16 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const goals = [];
+    const goals = [
+      { id: 0, title: 'Goal 0' },
+      { id: 11, title: 'Goal 1' },
+      { id: 12, title: 'Goal 2' },
+      { id: 13, title: 'Goal 3' },
+      { id: 14, title: 'Goal 4' },
+      { id: 15, title: 'Goal 5' },
+      { id: 16, title: 'Goal 6' },
+      { id: 17, title: 'Goal 7' }
+    ]
     return {goals};
   }
 }

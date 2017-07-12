cba26d5f43c153f2a1f9a487666ec25b36d883ea
Vinnie Vendemia <vvendemia1@gmail.com>
2017-07-11 21:48:24 -0400

    Update goal-service to use HTTP

--- src/app/services/goal.service.ts
+++ src/app/services/goal.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Goal } from '../models/goal'
-import { GOALS } from '../models/mocks/mock-goals'
 
 @Injectable()
 export class GoalService {
+
+  private goalsUrl = 'api/goals';  // URL to web api
+
+  constructor(private http: Http) { }
+
 	getGoals(): Promise<Goal[]> {
-		return Promise.resolve(GOALS);
+    return this.http.get(this.goalsUrl)
+      .toPromise()
+      .then(response => response.json().data as Goal[])
+      .catch(this.handleError)
 	}
 
 	getGoal(id: number): Promise<Goal> {
@@ -15,5 +26,11 @@ export class GoalService {
 
   addGoal(): Promise<String> {
     return Promise.resolve(`Successfully created new goal`);
+  }
+
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }

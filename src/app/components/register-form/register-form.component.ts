2e86f5bc32f11818e3e998bb37a0a2440513d22e
vvendemia <vvendemia1@gmail.com>
2017-08-11 07:54:07 -0400

    Attempt to re-add password validator to form group

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordValidation } from '../../validations/password_validation';
 
 import { User }    from '../../models/user';
 
@@ -11,9 +13,22 @@ import { User }    from '../../models/user';
 
 export class RegisterFormComponent {
 
+  form: FormGroup;
+  
 	constructor(
-    private router: Router
-  ) {}
+    private router: Router,
+    fb: FormBuilder,
+  ) {
+    this.form = fb.group(
+      {
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validator: PasswordValidation.MatchPassword
+      }
+    )
+  }
 	
   model = new User(1, 'Username', 'Password');
   submitted = false;
